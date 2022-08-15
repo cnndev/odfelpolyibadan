65d444c8e7395ea206c1d1c7c78cc36d49dc7787
Chihurumnanya Nwanevu <cnndev@Chihurumnanyas-MacBook-Pro.local>
2022-08-15 01:31:42 +0100

    Third Commit

--- js/main.js
+++ js/main.js
@@ -412,6 +412,25 @@ $(document).ready(function() {
         }
     }
 
+    // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
     //------- Mailchimp js --------//  
 
     $(document).ready(function() {
